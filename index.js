07959490dc2f1828dfbad9b468dc60ee9947a85e
Jerome Decinco <jerome.decinco@maxwell.mi.it>
2023-04-27 16:50:23 +0200

    update

--- index.js
+++ index.js
@@ -1,17 +1,39 @@
 import { Configuration, OpenAIApi } from "openai";
+import express from "express";
+import bodyParser from "body-parser";
+import cors from "cors";
 
 const configuration = new Configuration({
-    organization: "org-48Z5lXhi5J9bSUphxwolUJQL",
-    apiKey: "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",
+    organization: "org-Iz3Bg0czL1UaWpWE2LWMc6Wk",
+    apiKey: "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",
 });
 
 const openai = new OpenAIApi(configuration);
 
-const completion = await openai.createChatCompletion ({
-    model: "text-davinci-003",
-    messages: [
-        {role: "user", content: "Hello World"},
-    ]
-})
+const app = express();
+const port = 3000;
 
-console.log(completion.data.choices[0].message);
+app.use(bodyParser.json());
+
+app.post("/", async (req, res) => {
+
+    const { message } = req.body;
+
+    const completion = await openai.createChatCompletion ({
+        model: "gpt-3.5-turbo",
+        messages: [
+            {role: "user", content: `${message}`},
+        ]
+    })
+
+    res.json({
+        completion: completion.data.choices[0].message
+    })
+});
+
+app.listen(port, () => {
+    console.log(`Example app listening at http://localhost:${port}`);
+});
+
+
+
