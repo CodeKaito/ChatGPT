4bec3e5c2981f1ca294389ccec72683b18a0e555
Jerome Decinco <jerome.decinco@maxwell.mi.it>
2023-04-28 07:49:48 +0200

    Update index.js

--- index.js
+++ index.js
@@ -23,6 +23,7 @@ app.post("/", async (req, res) => {
     const completion = await openai.createChatCompletion ({
         model: "gpt-3.5-turbo",
         messages: [
+            {"role": "system", "content": "You are a helpful assistant."},
             {role: "user", content: `${message}`},
         ]
     })
